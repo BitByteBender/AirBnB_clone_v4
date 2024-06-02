8c5f3fa114970850375a4728646b741a5467861d
Soufiane Sadgali <ssadgali2@gmail.com>
2024-06-02 08:31:10 +0100

    Filter

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -37,28 +37,28 @@ $(document).ready(function () {
       url: 'http://0.0.0.0:5001/api/v1/places_search',
       method: 'POST',
       contentType: 'application/json',
-      dt: JSON.stringify({})
-    }).done(function (dt) {
+      data: JSON.stringify({})
+    }).done(function (data) {
       data.forEach(place => {
         const article = document.createElement('article');
 	article.innerHTML = `
-	  <div class="title_box">
-	    <h2>{{ place.name }}</h2>
-	    <div class="price_by_night">${{ place.price_by_night }}</div>
-	  </div>
-	  <div class="information">
-	    <div class="max_guest">
-	    {{ place.max_guest }} Guest{% if place.max_guest != 1 %}s{% endif %}</div>
-            <div class="number_rooms">
-	    {{ place.number_rooms }} Bedroom{% if place.number_rooms != 1 %}s{% endif %}</div>
-            <div class="number_bathrooms">
-	    {{ place.number_bathrooms }} Bathroom{% if place.number_bathrooms != 1 %}s{% endif %}</div>
-	  </div>
-	  <div class="user">
-            <b>Owner:</b> {{ place.user.first_name }} {{ place.user.last_name }}
+          <div class="title_box">
+          <h2>${place.name}</h2>
+          <div class="price_by_night">$${place.price_by_night}</div>
+          </div>
+          <div class="information">
+          <div class="max_guest">
+          ${place.max_guest} Guest${place.max_guest != 1 ? 's' : ''}</div>
+          <div class="number_rooms">
+          ${place.number_rooms} Bedroom${place.number_rooms != 1 ? 's' : ''}</div>
+          <div class="number_bathrooms">
+          ${place.number_bathrooms} Bathroom${place.number_bathrooms != 1 ? 's' : ''}</div>
+          </div>
+          <div class="user">
+          <b>Owner:</b> ${place.user.first_name} ${place.user.last_name}
           </div>
           <div class="description">
-	    {{ place.description | safe }}
+          ${place.description | safe}
           </div>`;
         $('section.places').append(article);
     }
